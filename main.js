4d3232ca4461b48bc73c791f93ff77c895df5cb5
ArenVosk <84827064+ArenVosk@users.noreply.github.com>
2021-08-04 11:54:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Aren ";
+var last_name = "Voskanyan";
+var full_name = first_name.concat(last_name);
+console.log("Hi I am Coding In Console Screen Where You Currently Are");
+console.log(full_name);
+
+var upercase_name = "AREN VOSKANYAN"
+var lowercase_name = upercase_name.toLowerCase();
+console.log(lowercase_name);
